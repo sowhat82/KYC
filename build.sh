cc0a4d1428f48135cb907bb86151f52fdb3458d4
Alvin <alvincheong@u.nus.edu>
2025-04-14 10:03:26 +0800

    Fix: install Python packages in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
-echo "DEBUG: Starting render-build.sh script"
-apt-get update && apt-get install -y tesseract-ocr
-echo "DEBUG: Finished installing Tesseract"
+#!/bin/bash
+
+set -o errexit
+
+apt-get update
+apt-get install -y tesseract-ocr
+
+pip install --upgrade pip
+pip install -r requirements.txt
