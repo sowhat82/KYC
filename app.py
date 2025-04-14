4e271be2645e669f6fb840c23c7003bc98071837
Alvin <alvincheong@u.nus.edu>
2025-04-14 08:56:09 +0800

    Force execution of render-build.sh in app.py

--- app.py
+++ app.py
@@ -205,6 +205,9 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
     app.run(host='0.0.0.0', port=port, debug=True)
 
+import os
+os.system("bash render-build.sh")
+
 {
   "python.envFile": "${workspaceFolder}/.env"
 }
